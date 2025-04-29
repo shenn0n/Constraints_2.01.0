ee7027bceb9e1cfc3256ba308e23e53530016e80
Alexandr Manzhosov <shenn0n4412@gmail.com>
2025-04-29 10:33:05 +0300

    initial commit

---
+++ Constraints_2.01.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Constraints_2.01.0
+//
+//  Created by Александр Манжосов on 29.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
